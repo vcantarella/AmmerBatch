36cd151a25d528028d4d6d4f02c8bc0e1ec25d59
vcantarella <vcantarella@gmail.com>
2025-04-28 16:31:29 +0200

    introducing more data from second experiment

--- scripts/fit_linear_regression.jl
+++ scripts/fit_linear_regression.jl
@@ -40,3 +40,40 @@ for (i,sample) in enumerate(samples)
 end
 late_times_params
 CSV.write(datadir("exp_pro","linear_regression_params.csv"), late_times_params)
+
+
+colors = [:blue, :green, :red]
+meas_names = ["NO3-", "DOC", "SO4-2"]
+
+R = 0.0821 # atm L / mol K
+T = 298.15 # K
+Kh = 0.025 # mol L⁻¹ atm⁻¹ henry's law constant
+df_info = CSV.read(datadir("exp_pro","sample_info_part2.csv"), DataFrame)
+t_change = 1
+late_times_params = DataFrame(sample = String[], k_no3 = Float64[], c₀ = Float64[], R² = Float64[], t_zeroorder = Float64[])
+samples = df_info[!, :Sample]
+for (i,sample) in enumerate(samples)
+    df = CSV.read(datadir("exp_pro","$sample.csv"), DataFrame)
+    #t_change_i = t_change[i]
+    modelled_times = df[df[!, :t].≥t_change, :t]
+    modelled_no3 = df[df[!, :t].≥t_change, "NO3-"]
+    index = convert.(Bool, 1 .-(ismissing.(modelled_no3)))
+    modelled_no3 = modelled_no3[index].*1e-3 # convert to mol L⁻¹
+    modelled_times = modelled_times[index]
+    # preparing linear regression
+    X = [ones(size(modelled_times)) modelled_times]
+    y = modelled_no3
+    # linear regression and model fit results:
+    β = (X'X)\(X'y)
+    ϵ = y - X*β
+    s² = ϵ'ϵ/(length(y)-length(β))
+    σ² = s²*(length(y)-length(β))/length(y)
+    ȳ = mean(y)
+    SST = sum((y .- ȳ).^2)
+    SSR = sum(ϵ.^2)
+    R² = 1 - SSR/SST
+    push!(late_times_params, [sample, -β[2], β[1], R², t_change_i])
+
+end
+late_times_params
+CSV.write(datadir("exp_pro","linear_regression_params.csv"), late_times_params)
--- scripts/reading_processing_plotting_raw_data.jl
+++ scripts/reading_processing_plotting_raw_data.jl
@@ -12,10 +12,6 @@ Currently active project is: $(projectname())
 Path of active project: $(projectdir())
 
 Have fun with your new project!
-
-You can help us improve DrWatson by opening
-issues on GitHub, submitting feature requests,
-or even opening your own Pull Requests!
 """
 )
 info = DataFrame(XLSX.readtable(datadir("exp_raw","johann_batch_preprocessed.xlsx"), "info"))
@@ -94,3 +90,68 @@ for sample in samples
     save(plotsdir("$(sample)_initial.png"), fig)
 end
 
+### Part 2 data processing
+# load the dataframes
+
+info_p2 = DataFrame(XLSX.readtable(datadir("exp_raw","johann_batch_preprocessed_part2.xlsx"), "info"))
+samples = info_p2[!, 1]
+t = DataFrame(XLSX.readtable(datadir("exp_raw","johann_batch_preprocessed_part2.xlsx"), "t (days)"))
+t = t[!, 1]
+# iterating over the measurements table and extracting the data as Dataframes.
+measurements = ["NO3 (mg L)", "DOC (mg L)", "SO4 (mg L)"]
+meas_names = ["NO3-", "DOC", "SO4-2",]
+molar_masses = [62.0049, 12.0107, 96.06,]
+colors = [:blue, :green, :red,]
+dfs = []
+for (j,measurement) in enumerate(measurements)
+    df = DataFrame(XLSX.readtable(datadir("exp_raw","johann_batch_preprocessed_part2.xlsx"), measurement,"A:M",header=false))
+    # change the column names
+    df = permutedims(df)
+    # set the first row as the column names
+    row1 = df[1, :]
+    df = df[2:end, :]
+    colnames = Symbol.([string(row1[i]) for i in 1:size(df, 2)])
+    rename!(df, colnames)
+    # replace missing values with NaNs
+    df = coalesce.(df, missing)
+    # divide by the molar masses
+    df = df ./ molar_masses[j] # mmol L⁻¹
+    push!(dfs, df)
+end
+# save info df to csv
+CSV.write(datadir("exp_pro","sample_info_part2.csv"), info_p2)
+# transpose the dataframes
+for sample in samples
+    df_sample = DataFrame(t = t)
+    for df in dfs
+        df_sample = hcat(df_sample, df[!,sample], makeunique=true)
+    end
+    rename!(df_sample, [Symbol("t");Symbol.(meas_names)])
+    CSV.write(datadir("exp_pro","$(sample).csv"), df_sample)
+    serialize(datadir("exp_pro","$(sample).jls"), df_sample)
+
+    # Plot the data for each sample
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel = "Time (days)", ylabel = "Concentration (mmol L⁻¹)",
+    title = "$(sample) initial data",
+    xticks = 0:10:170, yticks = 0:0.5:4,
+    xgridstyle = :dash, ygridstyle = :dash,
+    xgridwidth = 0.4, ygridwidth = 0.4,)
+    xlims!(ax,(-2, 180))
+    ylims!(ax,(-0.1, 4.1))
+    for (i, meas_name) in enumerate(meas_names)
+        missing_idx = findall(ismissing, df_sample[!,Symbol(meas_name)])
+        ts = df_sample[!,:t][setdiff(1:end, missing_idx)]
+        values = df_sample[setdiff(1:end, missing_idx), Symbol(meas_name)]
+        values = convert.(Float64, values)
+        if size(values, 1) == 0
+            continue
+        end
+        scatter!(ax, ts, values, label = meas_name, color = colors[i], markersize = 10,
+        marker = :diamond)
+        lines!(ax, ts, values, label = meas_name, color = colors[i], linestyle = :dash,
+        linewidth = 2)
+    end
+    axislegend(ax, position = :rt, merge = true)
+    save(plotsdir("$(sample)_initial.png"), fig)
+end
